d7c4a4b4560071b2516e47faca3082c486168055
Fernandespinheiro <147284420+Fernandespinheiro@users.noreply.github.com>
2025-03-25 22:24:46 -0300

    algumas mudanças

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from statistics import mean
 
 produtos = [{"nome":"hidratante","avaliacoes":[2,3,2,1]},
             {"nome":"sabonete","avaliacoes":[5,4,5,5]},
-            {"nome":"shampoo","avaliacoes":[3,4,5,2]}]
+            {"nome":"shampoo","avaliacoes":[3,4,5,2]}] #criando uma tabela de produtos
 
-produtoMedia =[{"A media é ": mean(produto["avaliacoes"])}for produto in produtos ]
+produtoMedia =[{"media": mean(produto["avaliacoes"])}for produto in produtos ] #fazendo a media
 
-filtro = (4)
+def criarFiltro(nota):
+    def filtrado(produto):
+        return produto["media"]>=nota
+    return filtrado 
+    
+filtro = criarFiltro(4) #closure
 
 def produtodoRecomendacao(produtos,filtro): return sorted(filter(filtro,produtos),key=lambda x: x["media"],reverse=True)
+recomedar = produtodoRecomendacao(produtoMedia,filtro) #parametro de recomendação
 
-print(produtodoRecomendacao)
+for produto in recomedar:
+    print(f"{produto['nome']}-media{produto['media']:.2f}") #Fim :)
